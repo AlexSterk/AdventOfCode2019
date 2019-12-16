310015b5a8525d7221a94d52b923b191ddc0a434
Alexander Sterk <a.j.h.sterk@student.tudelft.nl>
2019-12-16 17:22:07 +0100

    part 1 complete

--- AoC/src/Day16.java
+++ AoC/src/Day16.java
@@ -1,9 +1,11 @@
 import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,42 +14,83 @@ import java.util.stream.IntStream;
 public class Day16 {
     public static void main(String[] args) throws IOException {
         test();
-//        partOne();
+        partOne();
+//        partTwo();
     }
 
     private static void test() throws IOException {
+        int rounds = 100;
         String input = Files.readString(Paths.get("AoC/resources/day16-test.txt"));
-        List<Pair> in = IntStream.range(0, input.length()).mapToObj(i -> new Pair(i, Character.getNumericValue(input.charAt(i)))).collect(Collectors.toList());
-        List<Integer> pattern = List.of(0, 1, 0, -1);
-        List<Pair> out = new ArrayList<>();
-        for (int i = 0; i < in.size(); i++) {
-            int finalI = i;
-            List<Integer> finalPattern = pattern;
-            Pair nthPair = in.stream().map(p -> p.applyPattern(finalPattern)).reduce((pair, pair2) -> new Pair(finalI, pair.value + pair2.value)).get();
-            String value = Integer.toString(nthPair.value);
-            nthPair = new Pair(nthPair.index, Character.getNumericValue(value.charAt(value.length() - 1)));
-            out.add(nthPair);
+        List<Pair> out = runFFT(rounds, input);
 
-            LinkedList<Integer> nPattern = new LinkedList<>();
-            for (Integer integer : pattern) {
-                for (int j = 0; j < (i+1); j++) {
-                    nPattern.add(integer);
-                }
-            }
-            nPattern.remove();
-            pattern = nPattern;
+        System.out.println(out.stream().map(p -> Long.toString(p.value)).collect(Collectors.joining("")));
+    }
+
+    private static List<Pair> runFFT(int rounds, String input) {
+        List<Pair> in = IntStream.range(0, input.length()).mapToObj(i -> new Pair(i, Character.getNumericValue(input.charAt(i)))).collect(Collectors.toList());
+        final List<Integer> pattern = Collections.unmodifiableList(List.of(0, 1, 0, -1));
+
+        for (int i = 0; i < rounds; i++) {
+            in = getNewInput(in, pattern);
         }
-        System.out.println(out);
+        return in;
+    }
+
+    private static List<Pair> getNewInput(List<Pair> in, final List<Integer> originalPattern) {
+        List<Pair> out = new ArrayList<>();
+
+        return IntStream.range(0, in.size()).mapToObj(i -> {
+            List<Integer> pattern = getNewPattern(originalPattern, i);
+            return getNthPair(in, i, pattern);
+        }).collect(Collectors.toList());
+//
+//
+//        for (int i = 0; i < in.size(); i++) {
+//            pattern = getNewPattern(originalPattern, i);
+//            Pair nthPair = getNthPair(in, i, pattern);
+//            out.add(nthPair);
+//        }
+//        return out;
+    }
+
+    private static LinkedList<Integer> getNewPattern(List<Integer> pattern, int i) {
+        LinkedList<Integer> nPattern = new LinkedList<>();
+        for (Integer integer : pattern) {
+            for (int j = 0; j < (i+1); j++) {
+                nPattern.add(integer);
+            }
+        }
+        nPattern.add(nPattern.remove());
+        return nPattern;
+    }
+
+    private static Pair getNthPair(List<Pair> in, int finalI, List<Integer> pattern) {
+        Pair nthPair = in.stream().filter(p -> pattern.get(p.index % pattern.size()) != 0).map(p -> p.applyPattern(pattern)).reduce((pair, pair2) -> new Pair(finalI, pair.value + pair2.value)).get();
+        nthPair = new Pair(nthPair.index, Math.abs(nthPair.value) % 10);
+        return nthPair;
     }
 
     private static void partOne() throws IOException {
+        int rounds = 100;
         String input = Files.readString(Paths.get("AoC/resources/day16.txt"));
-        List<Integer> in = input.chars().mapToObj(Character::getNumericValue).collect(Collectors.toList());
+        List<Pair> out = runFFT(rounds, input);
+
+        System.out.println(out.stream().map(p -> Long.toString(p.value)).collect(Collectors.joining("")));
+    }
+
+    private static void partTwo() throws IOException {
+        int rounds = 1;
+        String input = Files.readString(Paths.get("AoC/resources/day16.txt")).repeat(10000);
+        List<Pair> out = runFFT(rounds, input);
+
+        System.out.println(out.stream().map(p -> Long.toString(p.value)).collect(Collectors.joining("")));
     }
 
     @AllArgsConstructor
+    @ToString
     public static class Pair {
-        public final int index, value;
+        public final int index;
+        public final int value;
 
         public Pair applyPattern(List<Integer> pattern) {
             int i = index % pattern.size();
@@ -55,6 +98,5 @@ public class Day16 {
 
             return new Pair(index, n);
         }
-
     }
 }
