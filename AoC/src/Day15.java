57a0723bfa3c17349e3d06460d56d69d4d893a42
Alexander Sterk <ajhsterk@gmail.com>
2019-12-16 05:45:15 +0100

    Day 15

--- AoC/src/Day15.java
+++ AoC/src/Day15.java
@@ -2,17 +2,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Day15 {
     public static class Node {
-        private static Node OXYGEN_SYSTEM;
-        public static final Set<Node> nodes = new HashSet<>();
+        public static Node OXYGEN_SYSTEM;
+        public static final Map<Node, Node> nodes = new HashMap<>();
         public static final List<List<Object>> moves = new ArrayList<>();
 
         public final int x, y;
         public final IntCodeInterpreter cpu;
-        private Map<DirectionY, Node> neighbours;
+        public final Set<Node> neighbours = new HashSet<>();
 
         public Node(int x, int y, IntCodeInterpreter cpu) {
             this.x = x;
@@ -32,45 +31,13 @@ public class Day15 {
                     continue;
                 }
                 Node node = new Node(x + dir.vx, y + dir.vy, cpu);
-                nodes.add(node);
+                nodes.putIfAbsent(node, node);
+                neighbours.add(nodes.get(node));
+                if (out == 2) OXYGEN_SYSTEM = node;
                 node.exploreNeighbours(visited, dir.opposite());
             }
             assert from == null || move(from) > 0;
         }
-
-//        void visitNeighbours(Node from) {
-//            if (neighbours == null) initNeighbours();
-//            for (Direction dir : neighbours.keySet()) {
-//                Node neighbour = neighbours.get(dir);
-//                if (neighbour == from) continue;
-//                assert move(dir) > 0;
-//                neighbour.visitNeighbours(this);
-//                assert neighbour.move(dir.opposite()) > 0;
-//            }
-//        }
-
-//        void initNeighbours() {
-//            if (neighbours != null) return;
-//            neighbours = new HashMap<>();
-//            for (Direction dir : Direction.values()) {
-//                Node potentialNeighbour = hasNeighbour(dir);
-//                if (potentialNeighbour != null) {
-//                    neighbours.put(dir, potentialNeighbour);
-//                    nodes.add(potentialNeighbour);
-//                }
-//            }
-//        }
-
-//        Node hasNeighbour(Direction dir) {
-//            long out = move(dir);
-//            boolean hasNeighbour = out == 1 || out == 2;
-//            Node node = new Node(x + dir.vx, y + dir.vy, cpu);
-//            if (out == 2) {
-//                OXYGEN_SYSTEM = node;
-//            }
-//            assert !hasNeighbour || node.move(dir.opposite()) > 0;
-//            return hasNeighbour ? node : null;
-//        }
 
         private int move(DirectionY dir) {
             cpu.in.offer((long) dir.command);
@@ -115,168 +82,38 @@ public class Day15 {
         IntCodeInterpreter cpu = new IntCodeInterpreter(program);
 
         Node position = new Node(0, 0, cpu);
-        Node.nodes.add(position);
-        try {
-            position.exploreNeighbours(new HashSet<>(), null);
-        } catch (AssertionError e) {
-            List<DirectionY> broken = Node.moves.stream().filter(l -> (long) l.get(2) > 0).map(l -> (DirectionY) l.get(1)).collect(Collectors.toList());
-            System.out.println(broken);
-            // [NORTH, NORTH, NORTH, NORTH, NORTH, NORTH, NORTH, NORTH, EAST, EAST, NORTH, NORTH, EAST, EAST, EAST, EAST]
-            IntCodeInterpreter cpu2 = new IntCodeInterpreter(program);
-            broken.forEach(d -> cpu2.in.offer((long) d.command));
-
-            cpu2.run();
-            System.out.println(cpu2.out);
-
-            cpu2.in.offer(1L);
-            cpu2.run();
-            System.out.println(cpu2.out);
-            cpu2.in.offer(2L);
-            cpu2.run();
-            System.out.println(cpu2.out);
-            cpu2.in.offer(3L);
-            cpu2.run();
-            System.out.println(cpu2.out);
-            cpu2.in.offer(4L);
-            cpu2.run();
-            System.out.println(cpu2.out);
-
-
-            e.printStackTrace();
-        }
+        Node.nodes.put(position, position);
+        position.exploreNeighbours(new HashSet<>(), null);
 
         System.out.println(Node.nodes);
+        System.out.println(Node.nodes.size());
+        System.out.println(Node.OXYGEN_SYSTEM);
 
-//        Map<Vector, Cell> map = new HashMap<>();
-//        Vector position = new Vector(0, 0);
-//
-//        Direction travelingIn = Direction.NORTH;
-//        map.put(position, Cell.EMPTY);
-
-//        cpu.in.offer((long) 1);
-//        cpu.in.offer((long) 2);
-//        cpu.in.offer((long) 1);
-//
-//        cpu.run();
-//        System.out.println(cpu.out);
-//
-//
-//        if (true) return;
-
-//
-//
-//
-//
-//        LinkedList<Direction> toExplore = new LinkedList<>();
-//        toExplore.offer(Direction.NORTH);
-//        toExplore.offer(Direction.EAST);
-//        toExplore.offer(Direction.SOUTH);
-//        toExplore.offer(Direction.WEST);
-//
-//        int backtrack = 0;
-//
-//        while (!toExplore.isEmpty()) {
-//            Direction moveIn = toExplore.poll();
-//
-//            Vector newPos = position.add(new Vector(moveIn.vx, moveIn.vy));
-//
-//            if (map.getOrDefault(newPos, Cell.EMPTY) == Cell.WALL) {
-//                continue;
-//            }
-//
-//            cpu.in.offer((long) moveIn.command);
-//            cpu.run();
-//            long out = cpu.out.pop();
-//
-//            if (out == 0) {
-//                map.putIfAbsent(newPos, Cell.WALL);
-//                continue;
-//            }
-//            if (out == 1) {
-//                map.putIfAbsent(newPos, Cell.EMPTY);
-//                position = newPos;
-//            }
-//            if (out == 2) {
-//                map.putIfAbsent(newPos, Cell.OXYGEN_SYSTEM);
-//                position = newPos;
-//            }
-//            toExplore.offer(Direction.WEST);
-//            toExplore.offer(Direction.SOUTH);
-//            toExplore.offer(Direction.EAST);
-//            toExplore.offer(Direction.NORTH);
-//        }
-//        drawMap(map, position);
-
-//        while (true) {
-//            drawMap(map, position);
-//            System.out.println();
-//            Vector newPos = position.add(new Vector(travelingIn.vx, travelingIn.vy));
-//
-//            if (map.getOrDefault(newPos, Cell.EMPTY) == Cell.WALL) {
-//                travelingIn = travelingIn.next();
-//                continue;
-//            }
-//
-//            if (map.containsKey(newPos)) {
-//                position = newPos;
-//                travelingIn = travelingIn.prev();
-//                continue;
-//            }
-//
-//            cpu.in.offer((long) travelingIn.command);
-//            cpu.run();
-//
-//            if (map.containsKey(newPos)) {
-//                position = newPos;
-//                travelingIn = travelingIn.prev();
-//                continue;
-//            }
-//
-//
-//            long output = cpu.out.pop();
-//
-//            if (output == 0) {
-//                map.putIfAbsent(newPos, Cell.WALL);
-//                travelingIn = travelingIn.next();
-//                continue;
-//            }
-//
-//            if (map.containsKey(newPos)) break;
-//
-//            if (output == 1) {
-//                map.putIfAbsent(newPos, Cell.EMPTY);
-//                position = newPos;
-//            }
-//            if (output == 2) {
-//                map.putIfAbsent(newPos, Cell.OXYGEN_SYSTEM);
-//                position = newPos;
-//            }
-//        }
-
+        int partOne = dijkstra(position, Node.nodes.keySet()).get(Node.OXYGEN_SYSTEM);
+        System.out.println(partOne);
+        int partTwo = Collections.max(dijkstra(Node.OXYGEN_SYSTEM, Node.nodes.keySet()).values());
+        System.out.println(partTwo);
     }
 
-    private static void drawMap(Map<Vector, Cell> map, Vector position) {
-        int minX = Collections.min(map.keySet(), Comparator.comparingInt(o -> o.get(0))).get(0);
-        int maxX = Collections.max(map.keySet(), Comparator.comparingInt(o -> o.get(0))).get(0);
-        int minY = Collections.min(map.keySet(), Comparator.comparingInt(o -> o.get(1))).get(1);
-        int maxY = Collections.max(map.keySet(), Comparator.comparingInt(o -> o.get(1))).get(1);
+    public static Map<Node, Integer> dijkstra(Node start, Set<Node> nodes) {
+        Map<Node, Integer> distances = new HashMap<>();
+        distances.put(start, 0);
+        Set<Node> toVisit = new HashSet<>(nodes);
+        toVisit.addAll(nodes);
 
-        Cell[][] mmap = new Cell[maxY - minY + 1][maxX - minX + 1];
-        map.forEach((v,c) -> mmap[v.get(1) - minY][v.get(0) - minX] = c);
+        while (!toVisit.isEmpty()) {
+            Node lowestDistance = Collections.min(toVisit, Comparator.comparing(n -> distances.getOrDefault(n, Integer.MAX_VALUE)));
+            toVisit.remove(lowestDistance);
 
-        for (int y = 0; y < mmap.length; y++) {
-            Cell[] row = mmap[y];
-            for (int x = 0; x < row.length; x++) {
-                Cell cell = row[x];
-                if (position.equals(new Vector(x + minX, y + minY))) System.out.print('.');
-                else if (cell == Cell.WALL) System.out.print('▓');
-                else if (cell == Cell.EMPTY) System.out.print('▒');
-                else if (cell == Cell.OXYGEN_SYSTEM) System.out.print('X');
-                else System.out.print('░');
-            }
-            System.out.println();
+            int distance = distances.getOrDefault(lowestDistance, Integer.MAX_VALUE);
+
+
+
+            lowestDistance.neighbours.forEach(n -> {
+                if (distance + 1 < distances.getOrDefault(n, Integer.MAX_VALUE)) distances.put(n, distance + 1);
+            });
         }
-
+        return distances;
     }
 
     public enum DirectionY {
@@ -314,11 +151,5 @@ public class Day15 {
         public DirectionY prev() {
             return next().opposite();
         }
-    }
-
-    public enum Cell {
-        WALL,
-        EMPTY,
-        OXYGEN_SYSTEM
     }
 }
