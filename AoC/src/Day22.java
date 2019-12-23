1038c5dc6e3a8b09e0919ba8ced8216e2b48356f
Alexander Sterk <ajhsterk@gmail.com>
2019-12-23 20:31:12 +0100

    fix 22. Turns out I was using test file for pt 2 oops

--- AoC/src/Day22.java
+++ AoC/src/Day22.java
@@ -7,7 +7,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -25,9 +24,7 @@ public class Day22 {
             if (Character.isDigit(s[3].charAt(0))) return new IncrementN(Integer.parseInt(s[3]));
             else return new DealNewStack();
         }).reduce(Combined::new).get();
-        List<Integer> factoryDeck = IntStream.range(0, 10).boxed().collect(Collectors.toList());
-        List<Integer> shuffled = shuffleStrategy.apply(factoryDeck);
-        System.out.println(shuffled);
+        System.out.println(shuffleStrategy.apply(IntStream.range(0, 10).boxed().collect(Collectors.toList())));
     }
 
     private static void partOne() throws IOException {
@@ -37,32 +34,25 @@ public class Day22 {
             if (Character.isDigit(s[3].charAt(0))) return new IncrementN(Integer.parseInt(s[3]));
             else return new DealNewStack();
         }).reduce(Combined::new).get();
-        List<Integer> factoryDeck = IntStream.range(0, 10007).boxed().collect(Collectors.toList());
-        List<Integer> shuffled = shuffleStrategy.apply(factoryDeck);
-        System.out.println(shuffled.indexOf(2019));
+        System.out.println(shuffleStrategy.apply(new BigInteger("2019"), new BigInteger("10007")));
     }
 
     private static void partTwo() throws IOException {
-        CardShuffle shuffleStrategy = Files.lines(Paths.get("AoC/resources/day22-test.txt")).map(line -> {
+        CardShuffle shuffleStrategy = Files.lines(Paths.get("AoC/resources/day22.txt")).map(line -> {
             String[] s = line.split(" ");
             if (s.length == 2) return new CutN(Integer.parseInt(s[1]));
             if (Character.isDigit(s[3].charAt(0))) return new IncrementN(Integer.parseInt(s[3]));
             else return new DealNewStack();
         }).reduce(Combined::new).get();
-//        System.out.println(((Combined)shuffleStrategy).first.apply(IntStream.range(0, 10).boxed().collect(Collectors.toList())));
-//        System.out.println(shuffleStrategy.apply(IntStream.range(0, 10).boxed().collect(Collectors.toList())));
-//        System.out.println(shuffleStrategy.apply(BigInteger.TWO, new BigInteger("10")));
-//        System.out.println(shuffleStrategy.inverseApply(new BigInteger("1"), new BigInteger("10")));
         
         var M = new BigInteger("119315717514047");
         var N = new BigInteger("101741582076661");
         var X = new BigInteger("2020");
-        var Y = shuffleStrategy.inverseApply(X, M);
-        var Z = shuffleStrategy.inverseApply(Y, M);
-        
-        var A = Y.subtract(Z).multiply(X.subtract(Y).modInverse(M)).mod(M);
-        var B = Y.subtract(A.multiply(X)).mod(M);
-        
+
+       
+        var B = shuffleStrategy.inverseApply(BigInteger.ZERO, M);
+        var A = shuffleStrategy.inverseApply(BigInteger.ONE, M).subtract(B).mod(M);
+
         var P = A.modPow(N, M);
         var PX = P.multiply(X);
         var P_1 = P.subtract(BigInteger.ONE);
@@ -71,27 +61,29 @@ public class Day22 {
     }
 }
 
-abstract class CardShuffle implements Function<List, List> {
+abstract class CardShuffle {
     public abstract BigInteger inverseApply(BigInteger a, BigInteger M);
     public abstract BigInteger apply(BigInteger x, BigInteger M);
+    
+    public List apply(List items) {
+        List t = new ArrayList(Collections.nCopies(items.size(), null));
+
+        for (Integer i = 0; i < items.size(); i++) {
+            t.set(apply(new BigInteger(i.toString()), new BigInteger(Integer.toString(items.size()))).intValue(), items.get(i));
+        }
+        return t;
+    }
 }
 
 class DealNewStack extends CardShuffle {
     @Override
     public BigInteger inverseApply(BigInteger a, BigInteger M) {
-        return M.subtract(a).subtract(BigInteger.ONE).mod(M);
+        return M.subtract(a).subtract(BigInteger.ONE);
     }
 
     @Override
     public BigInteger apply(BigInteger x, BigInteger M) {
         return M.subtract(BigInteger.ONE).subtract(x).mod(M);
-    }
-
-    @Override
-    public List apply(List list) {
-        List t = new ArrayList(list);
-        Collections.reverse(t);
-        return t;
     }
 }
 
@@ -108,14 +100,6 @@ class CutN extends CardShuffle {
     public BigInteger apply(BigInteger x, BigInteger M) {
         return x.subtract(new BigInteger(n.toString())).mod(M);
     }
-
-    @Override
-    public List apply(List list) {
-        int n = this.n < 0 ? list.size() + this.n : this.n;
-        ArrayList t = new ArrayList<>(list.subList(0, n));
-        t.addAll(list.subList(n, list.size()));
-        return t;
-    }
 }
 
 @Data
@@ -124,23 +108,13 @@ class IncrementN extends CardShuffle {
 
     @Override
     public BigInteger inverseApply(BigInteger a, BigInteger M) {
-        return new BigInteger(n.toString()).modInverse(M).multiply(a).mod(M);
+        BigInteger inverse = new BigInteger(n.toString()).modInverse(M);
+        return inverse.multiply(a).mod(M);
     }
 
     @Override
     public BigInteger apply(BigInteger x, BigInteger M) {
         return x.multiply(new BigInteger(n.toString())).mod(M);
-    }
-
-    @Override
-    public List apply(List list) {
-        List t = new ArrayList(Collections.nCopies(list.size(), -1));
-        int i = 0;
-        for (Object o : list) {
-            t.set(i, o);
-            i = (i + n) % list.size();
-        }
-        return t;
     }
 }
 
@@ -162,11 +136,5 @@ class Combined extends CardShuffle {
     public BigInteger apply(BigInteger x, BigInteger M) {
         BigInteger t = first.apply(x, M);
         return second.apply(t, M);
-    }
-
-    @Override
-    public List apply(List list) {
-        List t = first.apply(list);
-        return second.apply(t);
     }
 }
