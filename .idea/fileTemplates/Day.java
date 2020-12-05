235e8fa86b6dc02979e4c263db53ae751ce75987
Alexander Sterk <ajhsterk@gmail.com>
2020-12-05 23:30:31 +0100

    Add template

---
+++ .idea/fileTemplates/Day.java
@@ -0,0 +1,25 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+
+import setup.Day;
+
+public class ${NAME} extends Day {
+    @Override
+    public void processInput() {
+        
+    }
+
+    @Override
+    public void part1() {
+
+    }
+
+    @Override
+    public void part2() {
+
+    }
+
+    @Override
+    public int getDay() {
+        return ${PUZZLE_DAY};
+    }
+}
