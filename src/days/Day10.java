49643efca84c5849439faa0c31ede2d48ec121ce
Alexander Sterk <ajhsterk@gmail.com>
2020-12-10 13:27:12 +0100

    Day 10 p 2

--- src/days/Day10.java
+++ src/days/Day10.java
@@ -2,6 +2,7 @@ package days;
 
 import setup.Day;
 
+import java.math.BigInteger;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -27,13 +28,36 @@ public class Day10 extends Day {
             prev = number;
         }
 
-        System.out.println(counts);
+//        System.out.println(counts);
         System.out.println(counts.get(1) * (counts.get(3) + 1));
     }
 
     @Override
     public void part2() {
+        List<Integer> copy = new ArrayList<>(numbers);
+        copy.add(0);
+        copy.sort(Comparator.naturalOrder());
+        long[] DP = new long[copy.size()];
+        DP[0] = 1;
 
+        for (int i = 1; i < copy.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (copy.get(i) - copy.get(j) <= 3) DP[i] += DP[j];
+            }
+        }
+
+        System.out.println(DP[copy.size() - 1]);
+    }
+    
+    static boolean validChain(List<Integer> chain) {
+        int prev = 0;
+        
+        for (Integer number : chain) {
+            if (number - prev > 3) return false;
+            prev = number;
+        }
+        
+        return true;
     }
 
     @Override
