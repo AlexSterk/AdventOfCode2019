e1fb33433a546cf8a3dc0c29a4c0eebb370f4f14
Alexander Sterk <ajhsterk@gmail.com>
2020-12-14 16:17:29 +0100

    Day 14 part 2

--- src/days/Day14.java
+++ src/days/Day14.java
@@ -14,7 +14,7 @@ public class Day14 extends Day {
     
     private List<String> instructions;
     private List<Boolean> mask;
-    private Map<Integer, Long> memory;
+    private Map<Long, Long> memory;
 
     @Override
     public void processInput() {
@@ -32,7 +32,7 @@ public class Day14 extends Day {
                 List<Boolean> bits = longToBits(inst.value());
                 applyMask(bits);
                 long l = bitsToLong(bits);
-                memory.put(inst.key(), l);
+                memory.put((long) inst.key(), l);
             }
         }
 
@@ -41,7 +41,20 @@ public class Day14 extends Day {
 
     @Override
     public void part2() {
+        memory = new HashMap<>();
 
+        for (String instruction : instructions) {
+            if (instruction.startsWith("mask")) setMask(instruction.substring(7));
+            else {
+                Pair inst = stringToInstruction(instruction);
+                List<Boolean> bits = longToBits(inst.key());
+
+                List<Long> longs = applyFloatingMask(bits);
+                longs.forEach(l -> memory.put(l, (long) inst.value()));
+            }
+        }
+
+        System.out.println(memory.values().stream().mapToLong(l -> l).sum());
     }
 
     @Override
@@ -90,6 +103,33 @@ public class Day14 extends Day {
         for (int i = 0; i < mask.size(); i++) {
             if (mask.get(i) == null) continue;
             bits.set(i, mask.get(i));
+        }
+    }
+    
+    private List<Long> applyFloatingMask(List<Boolean> bits) {
+        for (int i = 0; i < mask.size(); i++) {
+            if (mask.get(i) != Boolean.FALSE) bits.set(i, mask.get(i));
+        }
+
+        ArrayList<List<Boolean>> acc = new ArrayList<>();
+        recursiveFloating(bits, acc);
+
+        return acc.stream().map(this::bitsToLong).collect(Collectors.toList());
+    }
+    
+    private void recursiveFloating(List<Boolean> bits, List<List<Boolean>> acc) {
+        int nullIndex = bits.indexOf(null);
+        if (nullIndex == -1) {
+            acc.add(bits);
+        } else {
+            var zero = new ArrayList<>(bits);
+            var one = new ArrayList<>(bits);
+            
+            zero.set(nullIndex, false);
+            one.set(nullIndex, true);
+            
+            recursiveFloating(zero, acc);
+            recursiveFloating(one, acc);
         }
     }
 
