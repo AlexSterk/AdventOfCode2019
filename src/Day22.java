9585ee19298116ec2b8a1a2220b3fb72f4be41a5
Alexander Sterk <a.j.h.sterk@student.tudelft.nl>
2020-12-22 12:21:33 +0100

    Day 22 p2

--- src/Day22.java
+++ src/Day22.java
@@ -3,7 +3,6 @@ import setup.Day;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.LongStream;
 
 public class Day22 extends Day {
     private List<Integer> p1Cards;
@@ -35,19 +34,71 @@ public class Day22 extends Day {
             }
         }
 
+        announceWinnerScore(playerOne, playerTwo);
+    }
+
+    private void announceWinnerScore(Queue<Integer> playerOne, Queue<Integer> playerTwo) {
         var winner = new ArrayList<>(playerOne.isEmpty() ? playerTwo : playerOne);
         Collections.reverse(winner);
-
         System.out.println(IntStream.range(0, winner.size()).map(i -> (i + 1) * winner.get(i)).sum());
     }
 
     @Override
     public void part2() {
+        recursiveGame(p1Cards, p2Cards, 0);
+    }
 
+    int recursiveGame(Collection<Integer> p1, Collection<Integer> p2, int level) {
+        Queue<Integer> playerOne = new LinkedList<>(p1);
+        Queue<Integer> playerTwo = new LinkedList<>(p2);
+
+        List<Integer> p1Hashes = new ArrayList<>();
+        List<Integer> p2Hashes = new ArrayList<>();
+
+        while (!playerOne.isEmpty() && !playerTwo.isEmpty()) {
+            if (p1Hashes.contains(playerOne.hashCode()) && p2Hashes.contains(playerTwo.hashCode())) {
+                return 1;
+            }
+            p1Hashes.add(playerOne.hashCode());
+            p2Hashes.add(playerTwo.hashCode());
+
+            Integer one = playerOne.poll();
+            Integer two = playerTwo.poll();
+
+            int winner;
+
+            if (playerOne.size() >= one && playerTwo.size() >= two) {
+                winner = recursiveGame(
+                        playerOne.stream().limit(one).collect(Collectors.toList()),
+                        playerTwo.stream().limit(two).collect(Collectors.toList()),
+                        level + 1);
+            } else {
+                winner = one > two  ? 1 : 2;
+            }
+
+            if (winner == 1) {
+                playerOne.offer(one);
+                playerOne.offer(two);
+            } else {
+                playerTwo.offer(two);
+                playerTwo.offer(one);
+            }
+        }
+
+        if (level == 0) {
+            announceWinnerScore(playerOne, playerTwo);
+        }
+
+        return playerOne.size() > playerTwo.size() ? 1 : 2;
     }
 
     @Override
     public int getDay() {
         return 22;
+    }
+
+    @Override
+    public boolean isTest() {
+        return false;
     }
 }
